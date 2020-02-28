22636f0ce23866be56cbf2ea87fd3ef7724e3067
Turtlefromocean <oliviajiyoungshin@gmail.com>
2020-02-28 17:27:25 +0900

    md 수정 및 오류 해결

--- Web/Flask_darkflow/server.py
+++ Web/Flask_darkflow/server.py
@@ -1,24 +1,21 @@
 from flask import Flask, url_for, render_template, Response
-import threading
-import numpy as np
-import cv2, time
-from darkflow.net.build import TFNet
+from darkflow.darkflow.net.build import TFNet
+import cv2
 import tensorflow as tf
+import threading
+import numpy
+
 
 app = Flask(__name__)
 
-options = {"model": "cfg/handlang-small.cfg",
-           "pbLoad": "darkflow/built_graph/handlang-small.pb",
-           "metaLoad": "darkflow/built_graph/handlang-small.meta" ,
-           "threshold": 0.1
-           }
-
-#options = {"model": "cfg/yolo.cfg", "load": "bin/yolo.weights", "threshold": 0.1}
+options = {"model": "./darkflow/cfg/handlang-small.cfg",
+           "pbLoad": "./darkflow/darkflow/built_graph/handlang-small.pb",
+           "metaLoad": "./darkflow/darkflow/built_graph/handlang-small.meta" , "threshold": 0.4}
 
 tfnet = TFNet(options)
 
-def gen(camera):
 
+def gen(camera):
     sess = tf.Session()
 
     with sess.as_default():
@@ -26,104 +23,46 @@ def gen(camera):
         while True:
 
             success, img = camera.read()
-            
+
             if success:
-                try:
                     results = tfnet.return_predict(img)
 
+
                     for result in results:
-                        tl= (result['topleft']['x'],result['topleft']['y'])
-                        br =(result['bottomright']['x'],result['bottomright']['y'])
-                        label = result['label']
-                        cv2.rectangle(img,tl,br,(0,255,0),3)
-                        cv2.putText(img,label,br,cv2.FONT_HERSHEY_COMPLEX, 2,(0,0,0),1)
-                    # cv2.imshow('frame',img)
-                        
+                        #tl = (result["topleft"]['x'], result['topleft']['y'])
+                        #br = (result['bottomright']['x'], result['bottomright']['y'])
+                        label = result["label"]
+                        print(label)
+                        cv2.rectangle(img,
+                                    (result["topleft"]["x"], result["topleft"]["y"]),
+                                    (result["bottomright"]["x"], result["bottomright"]["y"]),
+                                    (255, 0, 0), 4)
+                        text_x, text_y = result["topleft"]["x"] - 10, result["topleft"]["y"] - 10
+                        cv2.putText(img, result["label"], (text_x, text_y), cv2.FONT_HERSHEY_SIMPLEX,
+                                    0.8, (0, 255, 0), 2, cv2.LINE_AA)
+                    cv2.imshow('frame',img)
+
                     ret, jpeg = cv2.imencode('.jpg', img)
                     frame = jpeg.tobytes()
 
                     yield (b'--frame\r\n'
                            b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-                except:
-                  print("An exception occurred")
-
             else:
-                print("Status of camera.read()\n",success, img,"\n=======================")
-                
+                print("Status of camera.read()\n", success, img, "\n=======================")
 
-# @app.route('/video_feed')
-# def video_feed():
-#     cam = cv2.VideoCapture(0)
-#     # cam.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-#     # cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 360)
-#     if cam.isOpened():
-#         return Response(gen(cam),mimetype='multipart/x-mixed-replace; boundary=frame')
-
-@app.route('/video_stream')
-def video_stream():
-    camera = cv2.VideoCapture(0)
+@app.route('/video_feed')
+def video_feed():
+    cam = cv2.VideoCapture(0)
     # cam.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
     # cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 360)
+    #if cam.isOpened():
+    #    print('opended')
+    return Response(gen(cam),mimetype='multipart/x-mixed-replace; boundary=frame')
 
-    return Response(gen(camera), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/')
 def webcam():
-    return render_template('index.html')
+    return render_template('webcam.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
-
-
-#from flask import Flask, url_for, render_template, Response
-#import threading
-#import numpy
-#import cv2, time
-#from darkflow.net.build import TFNet
-#import tensorflow as tf
-#
-#app = Flask(__name__)
-#
-#options = {"model": "cfg/handlang-small.cfg",
-#           "pbLoad": 'darkflow/built_graph/handlang-small.pb',
-#           "metaLoad": 'darkflow/built_graph/handlang-small.meta' , "threshold": 0.1}
-#
-#tfnet = TFNet(options)
-#
-#def gen(camera):
-#    sess = tf.Session()
-#
-#    with sess.as_default():
-#
-#        while True:
-#            success, img = camera.read()
-#
-#            results = tfnet.return_predict(img)
-#            for result in results:
-#                tl= (result['topleft']['x'],result['topleft']['y'])
-#                br =(result['bottomright']['x'],result['bottomright']['y'])
-#                label = result['label']
-#                cv2.rectangle(img,tl,br,(0,255,0),3)
-#                cv2.putText(img,label,br,cv2.FONT_HERSHEY_COMPLEX, 2,(0,0,0),1)
-#            # cv2.imshow('frame',img)
-#
-#            ret, jpeg = cv2.imencode('.jpg', img)
-#            frame = jpeg.tobytes()
-#
-#            yield (b'--frame\r\n'
-#                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-#
-#@app.route('/video_feed')
-#def video_feed():
-#    cam = cv2.VideoCapture(0)
-#    # cam.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-#    # cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 360)
-#    return Response(gen(cam),mimetype='multipart/x-mixed-replace; boundary=frame')
-#
-#
-#@app.route('/')
-#def webcam():
-#    return render_template('webcam.html')
-#
-#if __name__ == '__main__':
-#    app.run(host='0.0.0.0', debug=False)
+    app.run(host='0.0.0.0', debug=False)
