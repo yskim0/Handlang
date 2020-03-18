8a12cda137338d48418ca65524b0d878d8d44d08
seohsj <hyunju6889@naver.com>
2020-03-18 10:48:36 +0900

    최종 수정

--- Web/flask_webteam/app.py
+++ Web/flask_webteam/app.py
@@ -14,7 +14,7 @@ tfnet = TFNet(options)
 predict_label = ''
 
 
-def get_alphabet_list(element):
+def get_alphabet_list():
     alphabet_list = ['person', 'teddybear', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
     
     return alphabet_list
